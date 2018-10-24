a69fdb664a9ec1017e56e14c9ac4c4bb104d6efd
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-10-23 20:02:27 -0500

    example of default values

---
+++ DefaultValues/src/com/company/Example.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Example {
+    boolean myBoolean;
+    char myChar;
+    byte myByte;
+    short myShort;
+
+    public boolean isMyBoolean() {
+        return myBoolean;
+    }
+
+    public char getMyChar() {
+        return myChar;
+    }
+
+    public byte getMyByte() {
+        return myByte;
+    }
+
+    public short getMyShort() {
+        return myShort;
+    }
+
+    public int getMyInteger() {
+        return myInteger;
+    }
+
+    public long getMyLong() {
+        return myLong;
+    }
+
+    public float getMyFloat() {
+        return myFloat;
+    }
+
+    public double getMyDouble() {
+        return myDouble;
+    }
+
+    int myInteger;
+    long myLong;
+    float myFloat;
+    double myDouble;
+
+
+}
---
+++ DefaultValues/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Example myExample = new Example();
+        System.out.println(myExample.isMyBoolean());
+        System.out.println(myExample.getMyChar());
+        System.out.println(myExample.getMyByte());
+        System.out.println(myExample.getMyShort());
+        System.out.println(myExample.getMyInteger());
+        System.out.println(myExample.getMyLong());
+        System.out.println(myExample.getMyFloat());
+        System.out.println(myExample.getMyDouble());
+    }
+}
