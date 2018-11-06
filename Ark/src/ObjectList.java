80796491e26575c2a3230698909b5096159ce86d
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-11-06 04:47:55 -0600

    The Perils of Polymorphic references of type Object

---
+++ Ark/src/ObjectList.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class ObjectList {
+
+    public static void main(String[] args) {
+//        The Perils of Polymorphic references of type Object
+        ArrayList<Object> animalArrayList = new ArrayList<Object>();
+        Dog a = new Dog();
+        animalArrayList.add(a);
+
+//        Everything comes out as a reference of type Object, so this won't work
+//        Dog b = animalArrayList.get(0);
+//        Yet, it is still class Dog
+        System.out.println("From the index, it's still a " + animalArrayList.get(0).getClass());
+        Object b = animalArrayList.get(0);
+        System.out.println("Object reference type, but it's still a " + b.getClass());
+
+//        Casting appears to be a decent workaround
+        Dog c = (Dog) animalArrayList.get(0);
+        System.out.println(c);
+        Dog d = (Dog) b;
+        System.out.println(d);
+    }
+}
