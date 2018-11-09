70c92affba531ed47358aa108882c34faaa2a795
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-11-09 02:38:40 -0600

    having fun with this()

--- Ark/src/Griffin.java
+++ Ark/src/Griffin.java
@@ -1,4 +1,7 @@
 public class Griffin extends AbstractAnimal{
+    public Griffin() {
+        this("unnamed");
+    }
     public Griffin(String name) {
         super(name);
     }
--- Ark/src/Main.java
+++ Ark/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
 //        Hippo hip = new Hippo();
         Griffin griff = new Griffin("Griff");
+        Griffin g = new Griffin();
         System.out.println(griff.getName());
+        System.out.println(g.getName());
     }
 }
