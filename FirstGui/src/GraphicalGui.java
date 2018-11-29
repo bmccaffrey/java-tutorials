7abd4e8b29818458d62fe77425072845f5b4e55a
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-11-29 04:10:21 -0600

    add method to draw JPEG

--- FirstGui/src/GraphicalGui.java
+++ FirstGui/src/GraphicalGui.java
@@ -7,8 +7,13 @@ public class GraphicalGui extends JPanel {
         gui.repaint();
     }
 
+//    public void paintComponent(Graphics g) {
+//        g.setColor(Color.orange);
+//        g.fillRect(20,50,100,100);
+//    }
+    
     public void paintComponent(Graphics g) {
-        g.setColor(Color.orange);
-        g.fillRect(20,50,100,100);
+        Image image = new ImageIcon("example.jpeg").getImage();
+        g.drawImage(image,3,4,this);
     }
 }
