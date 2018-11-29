6adab7907a999e279c650e6b883976b66ebe112c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-11-29 04:14:15 -0600

    add random fill colors

--- FirstGui/src/GraphicalGui.java
+++ FirstGui/src/GraphicalGui.java
@@ -7,13 +7,19 @@ public class GraphicalGui extends JPanel {
         gui.repaint();
     }
 
-//    public void paintComponent(Graphics g) {
-//        g.setColor(Color.orange);
-//        g.fillRect(20,50,100,100);
-//    }
-    
     public void paintComponent(Graphics g) {
-        Image image = new ImageIcon("example.jpeg").getImage();
-        g.drawImage(image,3,4,this);
+        g.fillRect(0,0,this.getWidth(), this.getHeight());
+        int red = (int) (Math.random() * 256);
+        int green = (int) (Math.random() * 256);
+        int blue = (int) (Math.random() * 256);
+        Color randomColor = new Color(red, green, blue);
+        g.setColor(randomColor);
+        g.fillOval(70,70,100,100);
     }
+
+//    public void paintComponent(Graphics g) {
+//        Image image = new ImageIcon("example.jpeg").getImage();
+//        g.drawImage(image,3,4,this);
+//    }
+
 }
