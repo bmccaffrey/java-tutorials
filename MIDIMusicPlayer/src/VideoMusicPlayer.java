b1b065aaf2b3263a03150da42f28ef9c4dc024ee
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-11-30 23:25:41 -0600

    create scale

--- MIDIMusicPlayer/src/VideoMusicPlayer.java
+++ MIDIMusicPlayer/src/VideoMusicPlayer.java
@@ -1,7 +1,24 @@
-import javax.sound.midi.MidiEvent;
-import javax.sound.midi.ShortMessage;
+import javax.sound.midi.*;
 
 public class VideoMusicPlayer {
+
+    public static void main(String[] args) {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            for (int i = 5; i < 61; i+=4) {
+                track.add(makeEvent(144, 1, i, 100, i));
+                track.add(makeEvent(144, 1, i, 100, i + 2));
+            }
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(220);
+            sequencer.start();
+        } catch (Exception ex) {ex.printStackTrace();}
+    }
 
     public static MidiEvent makeEvent(int command, int channel, int note, int velocity, int tick) {
         MidiEvent event = null;
