021f48b1c69be36af224e82f3be33e9a95f75fd0
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-11-30 22:50:25 -0600

    PSE WARNING: add animation

--- FirstGui/src/AnimationStation.java
+++ FirstGui/src/AnimationStation.java
@@ -1,47 +1,62 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class AnimationStation implements ActionListener {
-    JFrame frame;
+public class AnimationStation{
+    private JFrame frame;
+    private int[] coordinates = new int[2];
+    private int[] rgb = new int[3];
 
     public static void main(String[] args) {
         AnimationStation gui = new AnimationStation();
-        gui.setUp();
+        gui.setUpFrameAndPanel();
+        gui.painfullyAnimate(40, 50);
     }
-    private void setUp() {
+    private void setUpFrameAndPanel() {
         frame = new JFrame();
+        createPanel();
+        setFrameDefaults();
+    }
+
+    private void setFrameDefaults() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JButton button = new JButton("Move Stuff!");
-        button.addActionListener(this);
-        frame.getContentPane().add(BorderLayout.SOUTH, button);
-
-        MyDrawPanel panel = new MyDrawPanel();
-        frame.getContentPane().add(BorderLayout.CENTER, panel);
-
         frame.setSize(300,300);
         frame.setVisible(true);
     }
-    public void actionPerformed(ActionEvent event) { frame.repaint(); }
+
+    private void createPanel() {
+        MyDrawPanel panel = new MyDrawPanel();
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+    }
+
+    private void painfullyAnimate(int speedMillis, int duration) {
+        for(int i = 0; i < duration; i++) {
+            randomizeColorsAndCoordinates();
+            frame.repaint();
+            addDelay(speedMillis);
+        }
+    }
+
+    private void addDelay(int speedMillis) {
+        try { Thread.sleep(speedMillis); }
+        catch (Exception ex) { ex.printStackTrace();}
+    }
+
+    private void randomizeColorsAndCoordinates() {
+        randomizeValues(rgb, 256);
+        randomizeValues(coordinates, 175);
+    }
+
+    private void randomizeValues(int[] array, int upperBound) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * upperBound);
+        }
+    }
 
     class MyDrawPanel extends JPanel {
-        int[] coordinates = new int[2];
-        int[] rgb = new int[3];
-
-        public void randomizeValues(int[] array, int upperBound) {
-            for (int i = 0; i < array.length; i++) {
-                array[i] = (int) (Math.random() * upperBound);
-            }
-        }
-
         public void paintComponent(Graphics g) {
-            randomizeValues(rgb, 256);
-            randomizeValues(coordinates, 175);
             Color randomColor = new Color(rgb[0], rgb[1], rgb[2]);
             g.setColor(randomColor);
-            g.fillOval(coordinates[0], coordinates[1],100,100);
+            g.fillOval(coordinates[0], coordinates[1], 100, 100);
         }
     }
 
