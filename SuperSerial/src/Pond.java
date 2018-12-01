b241ee090bf61f858a1d98bcde30d3e6a5a14d0c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-01 06:19:25 -0600

    attempt to serialize w/o implementing Serializable

---
+++ SuperSerial/src/Pond.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Pond implements Serializable {
+
+    private Duck duck = new Duck();
+
+    public static void main(String[] args) {
+        Pond myPond = new Pond();
+
+        try {
+            FileOutputStream fs = new FileOutputStream("Pond.ser");
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(myPond);
+            os.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Duck {
+    public String bestAttribute = "feathers";
+    public boolean isImpedingTraffic = true;
+    public String sound = "Quack";
+
+    public void ruffle(String x) {
+        if (x.equalsIgnoreCase(bestAttribute)) { System.out.println(sound); }
+        else {
+            System.out.println("**Stare Blankly**");
+        }
+    }
+}
