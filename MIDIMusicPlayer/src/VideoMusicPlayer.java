83adc142ae1b0c716a60de23942149f846edae60
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-01 00:02:44 -0600

    sync display of random rectangles w/ random sounds

--- MIDIMusicPlayer/src/VideoMusicPlayer.java
+++ MIDIMusicPlayer/src/VideoMusicPlayer.java
@@ -1,25 +1,39 @@
 import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+// import java.io.*;
 
-public class VideoMusicPlayer implements ControllerEventListener {
+public class VideoMusicPlayer {
+    static JFrame frame = new JFrame("Fist & Only Music Video");
+    static MyDrawPanel ml;
 
     public static void main(String[] args) {
         VideoMusicPlayer vmp = new VideoMusicPlayer();
         vmp.go();
-        }
+    }
+
+    public void setUPGui() {
+        ml = new MyDrawPanel();
+        frame.setContentPane(ml);
+        frame.setBounds(30,30,300,300);
+        frame.setVisible(true);
+    }
 
     public void go() {
+        setUPGui();
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
 
-            int[] eventsIWant = {127};
-            sequencer.addControllerEventListener(this, eventsIWant);
-
+            sequencer.addControllerEventListener(ml, new int[] {127});
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
 
-            for (int i = 5; i < 61; i+=4) {
-                track.add(makeEvent(144, 1, i, 100, i));
+            int r = 0;
+            for (int i = 5; i < 61; i += 4) {
+                r = (int) ((Math.random() * 50) + 1);
+
+                track.add(makeEvent(144, 1, r, 100, i));
                 /*
                 Serves as Event for Sequencer
                 176 = ControllerEvent command;
@@ -27,16 +41,14 @@ public class VideoMusicPlayer implements ControllerEventListener {
                 Same Tick as NOTE ON, so fires on when each note start
                 */
                 track.add(makeEvent(176, 1, 127, 0, i));
-                track.add(makeEvent(144, 1, i, 100, i + 2));
+                track.add(makeEvent(128, 1, r, 100, i + 2));
             }
             sequencer.setSequence(seq);
-            sequencer.setTempoInBPM(220);
             sequencer.start();
-        } catch (Exception ex) {ex.printStackTrace();}
-    }
-
-    public void controlChange(ShortMessage event) {
-        System.out.println("la");
+            sequencer.setTempoInBPM(220);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public static MidiEvent makeEvent(int command, int channel, int note, int velocity, int tick) {
@@ -45,7 +57,37 @@ public class VideoMusicPlayer implements ControllerEventListener {
             ShortMessage a = new ShortMessage();
             a.setMessage(command, channel, note, velocity);
             event = new MidiEvent(a, tick);
-        } catch(Exception e) {e.printStackTrace();}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return event;
+    }
+
+    class MyDrawPanel extends JPanel implements ControllerEventListener {
+        boolean msg = false;
+
+        public void controlChange(ShortMessage event) {
+            msg = true;
+            repaint();
+        }
+
+        public void paintComponent(Graphics g) {
+            if (msg) {
+                Graphics2D g2 = (Graphics2D) g;
+                int r = (int) (Math.random() * 250);
+                int gr = (int) (Math.random() * 250);
+                int b = (int) (Math.random() * 250);
+
+                g.setColor(new Color(r, gr, b));
+
+                int ht = (int) ((Math.random() * 120) + 10);
+                int width = (int) ((Math.random() * 120) + 10);
+                int x = (int) ((Math.random() * 40) + 10);
+                int y = (int) ((Math.random() * 40) + 10);
+
+                g.fillRect(x, y, width, ht);
+                msg = false;
+            }
+        }
     }
 }
