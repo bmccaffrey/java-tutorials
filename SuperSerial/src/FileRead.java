b7b959e10ba7eea74221011d7882706be0923f28
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-01 08:13:50 -0600

    create simple FileReader class

---
+++ SuperSerial/src/FileRead.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead {
+    public static void main(String[] args) {
+        try {
+            File testFile = new File("test.txt");
+            FileReader fileReader = new FileReader(testFile);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = null;
+
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            
+            reader.close();
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
