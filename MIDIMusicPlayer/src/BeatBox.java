4e99d13cc4d7a22c846a56334456d3ea3981a504
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-20 06:33:21 -0600

    add rudimentary GUI

--- MIDIMusicPlayer/src/BeatBox.java
+++ MIDIMusicPlayer/src/BeatBox.java
@@ -1,5 +1,6 @@
 import javax.sound.midi.*;
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class BeatBox {
@@ -11,7 +12,18 @@ public class BeatBox {
     JFrame theFrame;
 
     public static void main(String[] args) {
-        new BeatBox().setUpMidi();
+        new BeatBox().buildGUI();
+    }
+
+    public void buildGUI() {
+        theFrame = new JFrame("Beatbox");
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        theFrame.setBounds(50, 50, 300, 300);
+        theFrame.setVisible(true);
     }
 
     public void setUpMidi() {
