93c069384a27fe40d94c91d5e2255307c70622f3
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-20 06:26:37 -0600

    add MIDI setup to beatbox class

---
+++ MIDIMusicPlayer/src/BeatBox.java
@@ -0,0 +1,27 @@
+import javax.sound.midi.*;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class BeatBox {
+    JPanel mainPanel;
+    ArrayList<JCheckBox> checkboxList;
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+    JFrame theFrame;
+
+    public static void main(String[] args) {
+        new BeatBox().setUpMidi();
+    }
+
+    public void setUpMidi() {
+        try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(120);
+        } catch(Exception e) { e.printStackTrace(); }
+    }
+
+}
