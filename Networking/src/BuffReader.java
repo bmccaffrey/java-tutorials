90d027382da64bbd97f1cbbb234443fe45fa9699
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-24 06:42:53 -0600

    create buffered reader connected to socket

---
+++ Networking/src/BuffReader.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class BuffReader {
+    public static void main(String[] args) {
+        BuffReader buff = new BuffReader();
+        buff.initialize();
+    }
+
+    public void initialize() {
+        try {
+            Socket socket = new Socket("127.0.0.1", 5000);
+            InputStreamReader stream = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(stream);
+            String message = reader.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
