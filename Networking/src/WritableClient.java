327f92105ce2e298de1a66e85770358cc2a5eba1
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-25 03:25:57 -0600

    add method to establish network connection

--- Networking/src/WritableClient.java
+++ Networking/src/WritableClient.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -23,10 +24,20 @@ public class WritableClient {
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        setUpNetworking();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 500);
         frame.setVisible(true);
     }
+
+    private void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1", 5000);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("Networking established");
+        } catch(IOException ex) { ex.printStackTrace(); }
+    }
+
     public class SendButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent ev) {
             try {
