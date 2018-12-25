2f26cb9a630c7429cc3b4d086ec4692f7f4872a6
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-25 03:22:03 -0600

    add action listener

--- Networking/src/WritableClient.java
+++ Networking/src/WritableClient.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -17,11 +19,22 @@ public class WritableClient {
         JPanel mainPanel = new JPanel();
         outgoing = new JTextField(20);
         JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 500);
         frame.setVisible(true);
+    }
+    public class SendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
+            } catch(Exception ex) { ex.printStackTrace(); }
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
     }
 }
