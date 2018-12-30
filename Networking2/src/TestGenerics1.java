e2fcc8639919d6122492e10098c228815dcf8ef7
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-30 14:24:39 -0600

    test ArrayList w/ supertype

--- Networking2/src/TestGenerics1.java
+++ Networking2/src/TestGenerics1.java
@@ -1,17 +1,28 @@
+import java.util.ArrayList;
+
 public class TestGenerics1 {
 
     public static void main(String[] args) {
         new TestGenerics1().go();
     }
     public void go() {
-        Animal [] animals = {new Dog(), new Cat(), new Dog()};
-        Dog[] dogs = {new Dog(), new Dog(), new Dog()};
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        animals.add(new Dog());
+        takeAnimals(animals);
+    }
+
+    public void takeAnimals(ArrayList<Animal> animals) {
+        for (Animal a: animals) {
+            a.eat();
+        }
     }
 
     abstract class Animal {
         void eat() { System.out.println("Animal eating."); }
     }
-    
+
     class Dog extends Animal {
         void bark() {}
     }
