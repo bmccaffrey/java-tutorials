27f31b506c13c946ae46258c715439debe09b70b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-30 06:20:51 -0600

    add some getters

--- Networking2/src/Jukebox1.java
+++ Networking2/src/Jukebox1.java
@@ -42,12 +42,29 @@ public class Jukebox1 {
         String rating;
         String bpm;
 
-        Song(String t, String a, String r, String b) {
-            title = t;
-            artist = a;
-            rating = r;
-            bpm = b;
+        public Song(String title, String artist, String rating, String bpm) {
+            this.title = title;
+            this.artist = artist;
+            this.rating = rating;
+            this.bpm = bpm;
         }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getArtist() {
+            return artist;
+        }
+
+        public String getRating() {
+            return rating;
+        }
+
+        public String getBpm() {
+            return bpm;
+        }
+        
     }
 
 }
