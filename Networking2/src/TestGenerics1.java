bc80d876070bae2b23eca8c089cefa2e77b1bfa9
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-30 14:31:51 -0600

    demonstration that Arrays have the same limitations

--- Networking2/src/TestGenerics1.java
+++ Networking2/src/TestGenerics1.java
@@ -1,29 +1,17 @@
-import java.util.ArrayList;
-
 public class TestGenerics1 {
 
     public static void main(String[] args) {
         new TestGenerics1().go();
     }
     public void go() {
-        ArrayList<Animal> animals = new ArrayList<Animal>();
-        animals.add(new Dog());
-        animals.add(new Cat());
-        animals.add(new Dog());
-
-        ArrayList<Dog> dogs = new ArrayList<Dog>();
-        dogs.add(new Dog());
-        dogs.add(new Dog());
-        dogs.add(new Dog());
-
+        Animal [] animals = {new Dog(), new Cat(), new Dog()};
+        Dog[] dogs = {new Dog(), new Dog(), new Dog()};
         takeAnimals(animals);
         takeAnimals(dogs);
     }
 
-    public void takeAnimals(ArrayList<Animal> animals) {
-        for (Animal a: animals) {
-            a.eat();
-        }
+    public void takeAnimals(Animal[] animals) {
+        animals[0] = new Cat();
     }
 
     abstract class Animal {
