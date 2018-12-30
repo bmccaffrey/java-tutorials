ca4dc15af81e3d399e136affc6cf097eeaccf832
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-30 14:35:33 -0600

    first implementation of a Wildcard

--- Networking2/src/TestGenerics1.java
+++ Networking2/src/TestGenerics1.java
@@ -1,17 +1,27 @@
+import java.util.ArrayList;
+
 public class TestGenerics1 {
 
     public static void main(String[] args) {
         new TestGenerics1().go();
     }
     public void go() {
-        Animal [] animals = {new Dog(), new Cat(), new Dog()};
-        Dog[] dogs = {new Dog(), new Dog(), new Dog()};
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        animals.add(new Dog());
+
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+
         takeAnimals(animals);
         takeAnimals(dogs);
     }
 
-    public void takeAnimals(Animal[] animals) {
-        animals[0] = new Cat();
+    public void takeAnimals(ArrayList<? extends Animal> animals) {
+        for (Animal a: animals) { a.eat(); }
     }
 
     abstract class Animal {
