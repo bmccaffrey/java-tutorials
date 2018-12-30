e2e4f92c7eca3f3ce304b44134054c7778e661ab
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-30 06:28:34 -0600

    use Song objects instead of strings and create a mess

--- Networking2/src/Jukebox1.java
+++ Networking2/src/Jukebox1.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 
 public class Jukebox1 {
 
-    ArrayList<String> songList = new ArrayList<String>();
+    ArrayList<Song> songList = new ArrayList<Song>();
 
     public static void main(String[] args) {
         new Jukebox1().go();
@@ -33,7 +33,9 @@ public class Jukebox1 {
 
     void addSong(String lineToParse) {
         String[] tokens = lineToParse.split("/");
-        songList.add(tokens[0]); // only adds the song title
+
+        Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
+        songList.add(nextSong);
     }
 
     class Song {
