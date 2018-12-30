a1152c2e03093bb10654aca3df7088c2a9a10a72
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-30 13:49:59 -0600

    create some instances, add to tree, get an error b/c I didn't implement Comparable

--- Networking2/src/TestTree.java
+++ Networking2/src/TestTree.java
@@ -1,8 +1,20 @@
+import java.util.TreeSet;
+
 public class TestTree {
     public static void main(String[] args) {
         new TestTree().go();
     }
-    public void go() {}
+    public void go() {
+        Book b1 = new Book("The Giving Tree");
+        Book b2 = new Book("The Fellowship of the Ring");
+        Book b3 = new Book("Quidditch Through the Ages");
+
+        TreeSet<Book> tree = new TreeSet<Book>();
+        tree.add(b1);
+        tree.add(b2);
+        tree.add(b3);
+        System.out.println(tree);
+    }
 
     class Book {
         String title;
