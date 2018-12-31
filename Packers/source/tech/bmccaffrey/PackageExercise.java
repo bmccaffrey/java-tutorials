a16fb0e511cb161f8f94e2c717ea4f6e18dac3a2
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-30 18:42:04 -0600

    create package w/ dummy class

---
+++ Packers/source/tech/bmccaffrey/PackageExercise.java
@@ -0,0 +1,13 @@
+package tech.bmccaffrey;
+
+public class PackageExercise {
+
+	public static void main(String[] args) {
+		new PackageExercise().doNothing();
+	}
+
+	public void doNothing() {
+		System.out.println("PackageExercise can and is doing nothing");
+		System.out.println("This is the extent of its functionality");
+	}
+}
