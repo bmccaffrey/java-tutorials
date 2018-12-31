d3b90202d0dc9891a3d8d1700191fbafe101b5b4
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-30 22:39:16 -0600

    add framework for methods

---
+++ BeatBoxFinal/src/BeatBoxFinal.java
@@ -0,0 +1,26 @@
+import javax.sound.midi.MidiEvent;
+import java.awt.event.ActionListener;
+
+public class BeatBoxFinal {
+
+
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga"};
+    int [] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+
+    public static void main(String[] args) {}
+    public void startUp(String name) {}
+    public void buildGUI() {}
+    public void setUpMidi() {}
+    public void buildTrackAndStart(){}
+    public class MyStartListener implements ActionListener {}
+    public class MyStopListener implements ActionListener {}
+    public class MyUpTempoListener implements ActionListener {}
+    public class MyDownTempoListener implements ActionListener {}
+    public class MySendListener implements ActionListener {}
+    public class MyListSelectionListener implements ActionListener {}
+    public class RemoteReader implements Runnable {}
+    public class MyPlayMineListener implements ActionListener {}
+    public void changeSequence() {}
+    public void makeTracks() {}
+    public MidiEvent makeEvent() {}
+}
